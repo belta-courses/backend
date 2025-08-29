ec637403cbfcef82864b9147a1930f957822dcd8
im-beltagy <imbeltagy@gmail.com>
2025-08-29 23:33:16 +0300

    chor: update eslit no unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   {
@@ -24,14 +25,32 @@ export default tseslint.config(
     },
   },
   {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unused-vars': 'off',
       'prettier/prettier': [
         'error',
         {
           endOfLine: 'auto',
+        },
+      ],
+      'unused-imports/no-unused-imports': 1,
+      'unused-imports/no-unused-vars': [
+        1,
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^ignore',
+          destructuredArrayIgnorePattern: '^_',
+          ignoreRestSiblings: true,
         },
       ],
     },
