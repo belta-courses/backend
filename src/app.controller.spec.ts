ea317b07fc0d3b181989540641a14c63bf2bbac2
im-beltagy <imbeltagy@gmail.com>
2025-08-29 05:24:50 +0300

    chor: init

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello Message!"', () => {
+      expect(appController.getHello()).toBe(
+        'Belta-Course server is working well! Thanks for checking❤️',
+      );
+    });
+  });
+});
---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Belta-Course server is working well! Thanks for checking❤️';
+  }
+}
