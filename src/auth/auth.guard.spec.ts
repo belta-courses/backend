82e7a35b42a72bb3d5ae62aebc883c11ef6cab4c
imbeltagy <imbeltagy@gmail.com>
2025-10-22 14:54:14 +0300

    refactor: remove non used dto

--- src/auth/auth.guard.spec.ts
+++ src/auth/auth.guard.spec.ts
@@ -1,7 +1,19 @@
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('AuthGuard', () => {
+  let guard: AuthGuard;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AuthGuard, { provide: JwtService, useValue: {} }],
+    }).compile();
+
+    guard = module.get<AuthGuard>(AuthGuard);
+  });
+
   it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
+    expect(guard).toBeDefined();
   });
 });
