bd2e5d85d92174a5cbe8584b62966e3cdebec30a
imbeltagy <imbeltagy@gmail.com>
2026-01-27 05:19:43 +0200

    fix(wallet): mail dependency error

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -4,11 +4,12 @@ import { WalletService } from './wallet.service';
 import { PrismaService } from 'src/prisma.service';
 import { PayPalModule } from 'src/paypal/paypal.module';
 import { UsersModule } from 'src/users/users.module';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   imports: [PayPalModule, UsersModule],
   controllers: [WalletController],
-  providers: [WalletService, PrismaService],
+  providers: [WalletService, PrismaService, MailService],
   exports: [WalletService],
 })
 export class WalletModule {}
